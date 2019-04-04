ee33f400371dee4367b557afb4bf02916c5756ef
mMmCode7 <mMm.Code@outlook.com>
2019-04-04 12:52:19 +0300

    Create Update_ExtensionAttribtute_For_List_of_AD_Users.ps1
    Provide the below information : 
    Excel Path in second line   # Example C:\UsersInfo.xlsx
    # fill the Excel file with the AD ID and attribute value

---
+++ Update_ExtensionAttribtute_For_List_of_AD_Users/Update_ExtensionAttribtute_For_List_of_AD_Users.ps1
@@ -0,0 +1,19 @@
+Install-Module ImportExcel
+
+$Excel_location = "Excel path"   # Example C:\UsersInfo.xlsx
+# excel must have two columns named as ( ADID -  Attribute )
+
+$users = Import-Excel $Excel_location
+
+$extensionAttributeName = "" #provide extensionattribute name example (extensionAttribute11)
+
+
+
+foreach($user in $users){
+
+    Set-ADUser -Identity $user.ADID -Clear $($extensionAttributeName)
+    Set-ADUser -Identity $user.ADID -Add @{$($extensionAttributeName) = "$($user.Attribute)"}
+
+    Write-Host "$user updated...." -ForegroundColor Magenta
+}
+
