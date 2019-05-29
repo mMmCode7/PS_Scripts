ea9c3376e32d923d6da23ffeec4a7d07cf990fe2
mMmCode7 <mMm.Code@outlook.com>
2019-05-29 11:58:59 +0300

    Create Inventory_list_of_Servers.ps1
    this will take input as text file with list of servers in each line with name Server_List.txt and output file with CSV formate named full report.csv, you can change the input and output location in the first and last line in the script.
    it will show the below information in the final report : 
    ComputerName - NumberOfCores - ProcessorThreadCount - SerialNumber - HDDSizein GB - RAMSizeinGB - OSArchitecture - OSVersion - SystemModel - systemManufacturer - IPaddress - OSName - NumberofEnabledCores - NumberOfLogicalProcessors - PysicalOrVirtual

---
+++ Inventory_list_of_Servers/Inventory_list_of_Servers.ps1
@@ -0,0 +1,43 @@
+
+$computers = get-content "$($env:USERPROFILE)\Desktop\Server_List.txt"
+$fullReport = @()
+
+foreach($Computer in $Computers){
+    write-host "Extracting information for HostName: $($Computer)" -ForegroundColor magenta
+    $OSObject = (Get-WmiObject -ComputerName $Computer -Class win32_operatingsystem|Select-Object *);
+    $SysObject = (Get-wmiobject -ComputerName $Computer -Class Win32_ComputerSystem|Select-Object *);
+    $BIOSObject = (Get-wmiobject Win32_BIOS|Select-Object *);
+    $ProcessorObject = (Get-wmiobject win32_Processor|Select-Object *);
+    $DiskSize = 0;
+    (Get-WmiObject Win32_LogicalDisk -Filter "DriveType='3'" |ForEach-Object{ $DiskSize+= ($_.Size)});
+    $RAMSizeInGB = 0;
+    [int] $RAMSizeInGB =  (($SysObject | Select-Object -ExpandProperty TotalPhysicalMemory)/1GB);
+    $IPaddress = Get-WmiObject win32_networkadapterconfiguration | Where-Object {$_.Ipenabled }|Select-object -ExpandProperty IPAddress
+    $Ipaddress = [system.String]::Join(" -- ", $IPaddress)
+    $win32_product = @(get-wmiobject -class ‘Win32_Product’ -computer $computer)
+        $properties = [ordered]@{
+            ComputerName = $app.PSComputerName
+            NumberOfCores = $ProcessorObject.NumberOfCores
+            ProcessorThreadCount = $ProcessorObject.ThreadCount
+            SerialNumber = $BIOSObject.SerialNumber
+            HDDSizeinGB = $DiskSize/1GB
+            RAMSizeinGB = $RAMSizeInGB
+            OSArchitecture = $OSObject | Select-Object -ExpandProperty OSArchitecture
+            OSVersion = $OSObject.Version
+            SystemModel = $SysObject.Model
+            SystemManufacturer = $SysObject.Manufacturer
+            IPaddress = $IPaddress
+            OSName = $OSObject.Caption
+            NumberOfEnabledCores = $ProcessorObject.NumberOfEnabledCore
+            NumebrOfLogicalProcessors = $ProcessorObject.NumberOfLogicalProcessors
+            PhysicalOrVirtual = $SysObject.Model
+        }          
+        $applications = New-Object PSObject -Property $properties
+
+       $fullReport += $applications
+       }
+
+
+
+
+$fullReport | export-csv "$($env:USERPROFILE)\Desktop\fullreport.csv"
